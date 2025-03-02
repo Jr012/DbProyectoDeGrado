5d06d817745ebd58a1fba4b06ac2f0c89382db76
Jr012 <rjose2529@gmail.com>
2025-03-02 00:29:42 -0500

    primer git

---
+++ src/test/java/com/example/DbProyectoDeGrado/DbProyectoDeGradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DbProyectoDeGrado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbProyectoDeGradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
